91ec0247da036722178abe929ac08720b5a21c51
estebanpetaflop <45972335+estebanpetaflop@users.noreply.github.com>
2019-02-12 10:13:29 +0100

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,6 @@ class Event < ApplicationRecord
 validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }, if: Proc.new{|a| a.duration%5==0}
 validates :title, presence: true, length: { in: 3 ..140 }
 validates :description, presence: true, length: { in: 20..1000 }
-validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 1001  }
 validates :location, presence: true, length: { minimum: 6 }
 end
